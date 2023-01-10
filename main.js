fe1f54411d52fcae5f7c0322f32a906092354cc4
DHIRAJ SINGHA <76531276+dhirajsingha143@users.noreply.github.com>
2023-01-10 14:40:56 +0100

    Resolved linters

--- main.js
+++ main.js
@@ -3,65 +3,64 @@ const form = document.getElementById('form-container');
 
 // Function to delete book from the list and local storage.
 const deleteBook = (btn) => {
-    const parent = btn.parentElement;
-    parent.parentElement.removeChild(parent);
-    let books = JSON.parse(localStorage.getItem('books'));
-    books = books.filter((book) => book.title !== btn.parentElement.children[0].innerHTML.slice(8));
-    localStorage.setItem('books', JSON.stringify(books));
+  const parent = btn.parentElement;
+  parent.parentElement.removeChild(parent);
+  let books = JSON.parse(localStorage.getItem('books'));
+  books = books.filter((book) => book.title !== btn.parentElement.children[0].innerHTML.slice(8));
+  localStorage.setItem('books', JSON.stringify(books));
 };
 
 const dispalyBooks = () => {
-    const books = JSON.parse(localStorage.getItem('books'));
-    booksContainer.innerHTML = '';
-    if (books) {
-        books.forEach((book) => {
-            if (book.title && book.author) {
-                const bookUI = document.createElement('div');
-                bookUI.classList.add('book');
-                bookUI.innerHTML = `
+  const books = JSON.parse(localStorage.getItem('books'));
+  booksContainer.innerHTML = '';
+  if (books) {
+    books.forEach((book) => {
+      if (book.title && book.author) {
+        const bookUI = document.createElement('div');
+        bookUI.classList.add('book');
+        bookUI.innerHTML = `
           <p class="book-title"> Title: ${book.title}</p>
           <div class= "book-author"> By ${book.author} </div>
           <button id = '${book.title}' type="button" class="remove-button"> Remove </button>
           `;
-                booksContainer.appendChild(bookUI);
-            }
-        });
-    }
-    const removeBtns = Array.from(document.querySelectorAll('.remove-button'));
-    removeBtns.forEach((btn) => btn.addEventListener('click', () => deleteBook(btn)));
+        booksContainer.appendChild(bookUI);
+      }
+    });
+  }
+  const removeBtns = Array.from(document.querySelectorAll('.remove-button'));
+  removeBtns.forEach((btn) => btn.addEventListener('click', () => deleteBook(btn)));
 };
 
 dispalyBooks();
 
 // Adds Book information to Local Storage
 const addToStore = (bookObj, title, author) => {
-    if (title && author) {
-        if (!localStorage.getItem('books')) {
-            localStorage.setItem('books', JSON.stringify([]));
-            const books = JSON.parse(localStorage.getItem('books'));
-            books.push(bookObj);
-            localStorage.setItem('books', JSON.stringify(books));
-        } else {
-            const books = JSON.parse(localStorage.getItem('books'));
-            books.push(bookObj);
-            localStorage.setItem('books', JSON.stringify(books));
-        }
+  if (title && author) {
+    if (!localStorage.getItem('books')) {
+      localStorage.setItem('books', JSON.stringify([]));
+      const books = JSON.parse(localStorage.getItem('books'));
+      books.push(bookObj);
+      localStorage.setItem('books', JSON.stringify(books));
+    } else {
+      const books = JSON.parse(localStorage.getItem('books'));
+      books.push(bookObj);
+      localStorage.setItem('books', JSON.stringify(books));
     }
+  }
 };
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const bookObj = {
-        title,
-        author,
-    };
-    form.elements.title.value = '';
-    form.elements.author.value = '';
+  event.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const bookObj = {
+    title,
+    author,
+  };
+  form.elements.title.value = '';
+  form.elements.author.value = '';
 
-    // Update the Storage and User Interface
-    addToStore(bookObj, title, author);
-    dispalyBooks();
-    
+  // Update the Storage and User Interface
+  addToStore(bookObj, title, author);
+  dispalyBooks();
 });
