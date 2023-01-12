7986782f26771bb7bf9a58843ae9395252b88d2b
DHIRAJ SINGHA <76531276+dhirajsingha143@users.noreply.github.com>
2023-01-12 10:27:31 +0100

    Add Navigational functionality

--- main.js
+++ main.js
@@ -88,6 +88,34 @@ addBtn.addEventListener('click', (e) => {
 
 const date = document.getElementById('date');
 // eslint-disable-next-line no-undef
-const { DateTime } = luxon;
-const now = DateTime.now();
-date.innerText = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+// const { DateTime } = luxon;
+// const now = DateTime.now();
+// date.innerText = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+
+//selecting buttons
+const listbtn = document.getElementById('link-list');
+const addbtn = document.getElementById('link-add');
+const contactbtn = document.getElementById('link-contact');
+//slecting containers
+const bc = document.getElementById('list');
+const ac = document.getElementById('add-book');
+const cc = document.getElementById('contact');
+
+//adding event listeners
+addbtn.addEventListener('click', () => {
+  bc.style.display = 'none';
+  ac.style.display = 'flex';
+  cc.style.display = 'none';
+});
+
+listbtn.addEventListener('click', () => {
+  bc.style.display = 'flex';
+  ac.style.display = 'none';
+  cc.style.display = 'none';
+});
+
+contactbtn.addEventListener('click', () => {
+  bc.style.display = 'none';
+  ac.style.display = 'none';
+  cc.style.display = 'flex';
+});
